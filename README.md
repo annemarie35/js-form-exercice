2bc8d9adbe0b3127335c28a9e8e8c66a5d9c3f05
Anne-Marie <amesteves@octo.com>
2020-03-04 10:55:30 +0100

    ajout d'un message dans la page

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Exercice simple d'initiation à javascript avec un formulaire de validation d'em
 - Récupérer le code sur sa machine
 `git clone git@github.com:annemarie35/js-form-exercice.git`
 - Décommenter les lignes 5 et 10 pour visualiser les boites de dialogues `alert()`
+
+# Etape 2
+
+Affichage d'un message en dessous du formulaire
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myForm = document.getElementById('myForm')
+let errorMessage
 
 myForm.addEventListener('submit', function(e) {
     // alert('Vous avez envoyé le formulaire !\n\nMais celui-ci a été bloqué pour que vous ne changiez pas de page.');
@@ -9,3 +10,6 @@ myForm.addEventListener('reset', function(e) {
     // alert('Vous avez réinitialisé le formulaire !')
     e.preventDefault()
 })
+
+errorMessage = "Message affiché"
+document.getElementById("error-message").innerHTML = errorMessage
